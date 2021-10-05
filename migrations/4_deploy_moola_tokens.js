ccf3cf785fdcac3af2a8af8324d72b7fbbfd029f
Brian Li <brnli7@gmail.com>
2021-10-05 07:57:44 -0700

    Fix mainnet Moola v2 addresses

--- migrations/4_deploy_moola_tokens.js
+++ migrations/4_deploy_moola_tokens.js
@@ -13,7 +13,7 @@ module.exports = function (deployer, network) {
           : '0x58ad305f1eCe49ca55ADE0D5cCC90114C3902E88'
       const mCELO =
         network === 'cmainnet'
-          ? '0x653cC2Cc0Be398614BAd5d5328336dc79281e246'
+          ? '0x7D00cd74FF385c955EA3d79e47BF06bD7386387D'
           : '0x653cC2Cc0Be398614BAd5d5328336dc79281e246'
       const CELO =
         network === 'cmainnet'
@@ -21,7 +21,7 @@ module.exports = function (deployer, network) {
           : '0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9'
       const mcUSD =
         network === 'cmainnet'
-          ? '0x3a0EA4e0806805527C750AB9b34382642448468D'
+          ? '0x918146359264C492BD6934071c6Bd31C854EDBc3'
           : '0x3a0EA4e0806805527C750AB9b34382642448468D'
       const cUSD =
         network === 'cmainnet'
@@ -29,7 +29,7 @@ module.exports = function (deployer, network) {
           : '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1'
       const mcEUR =
         network === 'cmainnet'
-          ? '0x0D9B4311657003251d1eFa085e74f761185F271c'
+          ? '0xE273Ad7ee11dCfAA87383aD5977EE1504aC07568'
           : '0x0D9B4311657003251d1eFa085e74f761185F271c'
       const cEUR =
         network === 'cmainnet'
