b1f25066c1ad38ea27e98e529ec756234cf39a81
zeph-HK <zephyr.zk@protonmail.com>
2021-12-02 17:32:58 +0000

    Update contracts for groth16

--- contracts/interfaces/IVerifier.sol
+++ contracts/interfaces/IVerifier.sol
@@ -3,5 +3,8 @@
 pragma solidity ^0.8.0;
 
 interface IVerifier {
-  function verifyProof(bytes calldata proof, uint[] calldata pubSignals) external view returns (bool);
+  function verifyProof(bytes memory proof, uint[3] memory pubSignals) external view returns (bool);
+  function verifyProof(bytes memory proof, uint[4] memory pubSignals) external view returns (bool);
+  function verifyProof(bytes memory proof, uint[5] memory pubSignals) external view returns (bool);
+  function verifyProof(bytes memory proof, uint[6] memory pubSignals) external view returns (bool);
 }
